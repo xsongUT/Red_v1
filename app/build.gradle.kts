784d77bde3799af00f7eba678c05f7f6e196e20f
xsongOU <122293901+xsongUT@users.noreply.github.com>
2025-04-14 11:56:31 -0400

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.red_v1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.red_v1"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled=true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+        viewBinding=true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.firebase:firebase-analytics:22.4.0")
+    implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.3")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.multidex:multidex:2.0.1")
+    implementation ("androidx.core:core:1.13.0")
+    implementation ("androidx.fragment:fragment:1.6.2")
+    implementation ("androidx.loader:loader:1.1.0")
+    implementation ("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+    implementation ("androidx.customview:customview:1.1.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+}
---
+++ app/src/main/java/com/example/red_v1/LoginActivity.kt
@@ -0,0 +1,127 @@
+package com.example.red_v1
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.activity.ComponentActivity
+
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.red_v1.ui.theme.Red_v1Theme
+import android.view.View
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.example.red_v1.databinding.ActivityLoginBinding
+import android.widget.Toast
+import android.widget.LinearLayout as LinearLayout1
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+    private val firestoreAuth = FirebaseAuth.getInstance()
+    private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
+       val user = firestoreAuth.currentUser?.uid
+       user?.let{
+            startActivity(HomeActivity.newIntent(this))
+           finish()
+       }
+    }
+    val emailET = findViewById<TextInputEditText>(R.id.emailET)
+    val emailTIL = findViewById<TextInputLayout>(R.id.emailTIL)
+    val passwordET = findViewById<TextInputEditText>(R.id.passwordET)
+    val passwordTIL = findViewById<TextInputLayout>(R.id.passwordTIL)
+    val loginProgressLayout = findViewById<LinearLayout1>(R.id.loginProgressLayout)
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onStart() {
+        super.onStart()
+        firestoreAuth.addAuthStateListener(firebaseAuthListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firestoreAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setTextChangeListener(emailET,emailTIL)
+        setTextChangeListener(passwordET,passwordTIL)
+
+        loginProgressLayout.setOnTouchListener { v, event -> true }
+
+    }
+
+    fun setTextChangeListener(et:EditText,til:TextInputLayout){
+        et.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                til.isErrorEnabled = false
+            }
+
+        })
+    }
+
+    fun onLogin(v:View){
+
+        var proceed =true
+        if(emailET.text.isNullOrEmpty()){
+                    emailTIL.error = "Email is required"
+                    emailTIL.isErrorEnabled = true
+                proceed=false
+            }
+        if(passwordET.text.isNullOrEmpty()){
+            passwordTIL.error = "Password is required"
+            passwordTIL.isErrorEnabled = true
+            proceed=false
+        }
+        if(proceed){
+            loginProgressLayout.visibility = View.VISIBLE
+            firestoreAuth.signInWithEmailAndPassword(emailET.text.toString(),passwordET.text.toString())
+                .addOnCompleteListener{task ->
+                    if(!task.isSuccessful){
+                        loginProgressLayout.visibility =View.GONE
+                        Toast.makeText(this@LoginActivity,"login error: ${task.exception?.localizedMessage}",Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnFailureListener{e ->
+                    e.printStackTrace()
+                    loginProgressLayout.visibility=View.GONE
+
+                }
+        }
+    }
+
+    fun goToSignup(v:View){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
