3a952ef74489701c40411dfcb4b97473581b3d9e
xsongOU <122293901+xsongUT@users.noreply.github.com>
2025-04-14 23:38:32 -0400

    3rd push

--- app/src/main/java/com/example/red_v1/activities/HomeActivity.kt
+++ app/src/main/java/com/example/red_v1/activities/HomeActivity.kt
@@ -8,6 +8,8 @@ import android.content.Context
 import android.view.View
 import com.example.red_v1.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.tabs.TabLayout
+
 
 
 
@@ -18,6 +20,19 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+
+//        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.selector_home))
+//        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.selector_search))
+//        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.selector_myactivity))
+        val tabHome = tabLayout.newTab().setIcon(R.drawable.selector_home)
+        val tabSearch = tabLayout.newTab().setIcon(R.drawable.selector_search)
+        val tabMyActivity = tabLayout.newTab().setIcon(R.drawable.selector_myactivity)
+
+        tabLayout.addTab(tabHome)
+        tabLayout.addTab(tabSearch)
+        tabLayout.addTab(tabMyActivity)
 
 
     }
